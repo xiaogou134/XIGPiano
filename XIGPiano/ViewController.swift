20ad8a0be851957714b191a0c65e8da27468d12b
Xiaogou134 <xiaogou134@hotmail.com>
2021-06-09 16:29:22 +0800

    Initial Commit

---
+++ XIGPiano/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XIGPiano
+//
+//  Created by xiaogou134 on 2021/6/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
